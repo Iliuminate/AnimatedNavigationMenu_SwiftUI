741fe438ebf94c447cd5ac747757e979def7a5a7
Carlos Enrique Diaz Arandia <cdiaz@merqueo.com>
2021-04-07 21:57:01 -0500

    Initial Commit

---
+++ CustomSideMenu_IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomSideMenu_IOS
+//
+//  Created by Carlos Diaz on 7/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomSideMenu_IOS/CustomSideMenu_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSideMenu_IOSApp.swift
+//  CustomSideMenu_IOS
+//
+//  Created by Carlos Diaz on 7/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CustomSideMenu_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
